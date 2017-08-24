5bf01cb9b6e3da5eb039b60e5d71cb8a99551189
Ganmani Sekar <18008751+kannu27@users.noreply.github.com>
2017-08-23 23:00:11 -0700

    Update BillTabFragment.java

--- app/src/main/java/com/sunlightfoundation/congressinfo/BillTabFragment.java
+++ app/src/main/java/com/sunlightfoundation/congressinfo/BillTabFragment.java
@@ -16,77 +16,66 @@ import android.widget.TableLayout;
  * Created by ganmanisekar on 11/16/16.
  */
 public class BillTabFragment extends Fragment {
-    public static TabLayout tabLayout;
-    public static ViewPager viewPager;
-    public static int int_items = 2 ;
+ public static TabLayout tabLayout;
+ public static ViewPager viewPager;
+ public static int int_items = 2;
 
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+ @Nullable
+ @Override
+ public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View x =  inflater.inflate(R.layout.bill_tab_layout,null);
-        tabLayout = (TabLayout) x.findViewById(R.id.bill_tabs);
-        viewPager = (ViewPager) x.findViewById(R.id.bill_viewpager);
+  View x = inflater.inflate(R.layout.bill_tab_layout, null);
+  tabLayout = (TabLayout) x.findViewById(R.id.bill_tabs);
+  viewPager = (ViewPager) x.findViewById(R.id.bill_viewpager);
+  viewPager.setAdapter(new MyAdapter(getChildFragmentManager()));
+  tabLayout.post(new Runnable() {
+   @Override
+   public void run() {
+    tabLayout.setupWithViewPager(viewPager);
+   }
+  });
+  return x;
 
-
-        viewPager.setAdapter(new MyAdapter(getChildFragmentManager()));
-
-
-
-        tabLayout.post(new Runnable() {
-            @Override
-            public void run() {
-                tabLayout.setupWithViewPager(viewPager);
-            }
-        });
-
-
-        
-        return x;
-
-    }
+ }
 
 
 
-    class MyAdapter extends FragmentPagerAdapter{
+ class MyAdapter extends FragmentPagerAdapter {
 
-        public MyAdapter(FragmentManager fm) {
+  public MyAdapter(FragmentManager fm) {
 
-            super(fm);
-        }
+   super(fm);
+  }
 
-        @Override
-        public Fragment getItem(int position)
-        {
-            switch (position){
-                case 0 : return new Bills();
-                case 1 : return new BillsNew();
-            }
-            return null;
-        }
+  @Override
+  public Fragment getItem(int position) {
+   switch (position) {
+    case 0:
+     return new Bills();
+    case 1:
+     return new BillsNew();
+   }
+   return null;
+  }
 
-        @Override
-        public int getCount() {
+  @Override
+  public int getCount() {
 
-            return int_items;
+   return int_items;
 
-        }
+  }
 
+  @Override
+  public CharSequence getPageTitle(int position) {
 
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-
-            switch (position){
-                case 0 :
-                    return "Active Bills";
-                case 1 :
-                    return "New Bills";
-
-
-            }
-            return null;
-        }
-    }
+   switch (position) {
+    case 0:
+     return "Active Bills";
+    case 1:
+     return "New Bills";
+   }
+   return null;
+  }
+ }
 }
